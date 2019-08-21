c987299c99bea549433d3f1fb525204ce11fd1d9
Ganing <sht_jx@sina.com>
2019-08-21 15:39:18 +0800

    fix:add missing files

---
+++ utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <time.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+time_t cvt_TIME(char const *Date);
+
+
+#ifdef  __cplusplus
+}
+#endif
+#endif // UTILS_H
